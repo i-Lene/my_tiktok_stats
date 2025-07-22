6718588d34cbc858179c6f3437e9f5913479659f
i-Lene <marlene.s.veiga@gmail.com>
2025-07-22 15:37:36 +0100

    fix2

--- backend/api_helper.py
+++ backend/api_helper.py
@@ -8,23 +8,35 @@ load_dotenv()
 ms_token = os.getenv("MS_TOKEN")
 
 
-
 async def myTikTokSatus():
-    async with TikTokApi(no_browser=True) as api:
-        user = await api.user("gingerflwrs").info()
+    async with TikTokApi() as api:
+        await api.create_sessions(
+            ms_tokens=[ms_token],
+            num_sessions=1,
+            sleep_after=3,
+            browser='chromium',
+            headless=False
+        )
 
-        videos = await api.user("gingerflwrs").videos(count=10000000)
+        user = api.user("gingerflwrs")
+        user_data = await user.info()
 
-        return {
-            "username": user["userInfo"]["user"]["uniqueId"],
-            "nickname": user["userInfo"]["user"]["nickname"],
-            "videoCount": user["userInfo"]["stats"]["videoCount"],
-            "heartCount": user["userInfo"]["stats"]["heartCount"],
-            "friendCount": user["userInfo"]["stats"]["friendCount"],
-            "followerCount": user["userInfo"]["stats"]["followerCount"],
-            "followingCount": user["userInfo"]["stats"]["followingCount"],
-            "avatar": user["userInfo"]["user"]["avatarThumb"],
-            "description": user["userInfo"]["user"]["signature"],
-            "videos": [video.as_dict if hasattr(video, "as_dict") else dict(video) for video in videos]
-        }
+        videos = []
 
+        async for video in user.videos(count=100000):
+            videos.append(video.as_dict)
+
+    status_dict = {
+        "username": user_data["userInfo"]["user"]["uniqueId"],
+        "nickname": user_data["userInfo"]["user"]["nickname"],
+        "videoCount": user_data["userInfo"]["stats"]["videoCount"],
+        "heartCount": user_data["userInfo"]["stats"]["heartCount"],
+        "friendCount": user_data["userInfo"]["stats"]["friendCount"],
+        "followerCount": user_data["userInfo"]["stats"]["followerCount"],
+        "followingCount": user_data["userInfo"]["stats"]["followingCount"],
+        "avatar": user_data["userInfo"]["user"]["avatarLarger"],
+        "description": user_data["userInfo"]["user"]["signature"],
+        "videos": videos
+    }
+
+    return status_dict

afca492204caf2b29f852a52bc9fdb7050ea33cf
i-Lene <marlene.s.veiga@gmail.com>
2025-07-31 18:06:51 +0100

    cron

--- backend/main.py
+++ backend/main.py
@@ -184,7 +184,7 @@ async def run_data_collection():
 def schedule_daily_collection():
     scheduler = BackgroundScheduler()
     scheduler.add_job(lambda: asyncio.run(
-        run_data_collection()), 'cron', hour=18, minute=5)
+        run_data_collection()), 'cron', hour=18, minute=20)
     scheduler.start()
 
 

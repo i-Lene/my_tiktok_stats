28010c98ed51df7d9ce0f3ef54c4da2a5821e053
i-Lene <marlene.s.veiga@gmail.com>
2025-08-04 15:52:20 +0100

    correct axios 2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://my-tiktok-stats-1.onrender.com",
+  baseURL: "https://my-tiktok-stats.onrender.com",
 });
 
 export default api;
